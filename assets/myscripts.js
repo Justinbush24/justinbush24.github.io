f37273b357c88a0d9c28b762e77c5be9abcc53eb
Justin Bush <justinbush24@gmail.com>
2020-10-24 10:20:24 -0500

    initial push

---
+++ assets/myscripts.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = getElementsByClassName(".myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
